4082db1e4273b69d1f97992f186cec3e4601f999
arjun380 <82092827+arjun380@users.noreply.github.com>
2021-09-03 16:01:56 +0530

    very smallllllll work

--- main.js
+++ main.js
@@ -16,18 +16,32 @@ recognition.onresult = function run (event) {
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
-    console.log(Content); 
-    speak();
+    console.log(Content);
+    
+    if(Content =="take my selfie")
+    {
+        console.log("taking your selfie");
+        speak();
+    }
+    
 }
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+
+        take_snapshot();
+        save();
+    },5000);
 }
 
 Webcam.set(
@@ -38,3 +52,17 @@ Webcam.set(
         png_quality:90
     });
     camera = document.getElementById("camera");
+
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">'; 
+        });
+    }
+
+    function save(){
+        link = document.getElementById("link");
+        image = document.getElementById("selfie_image").src;
+        link.href = image;
+        link.click();
+    }
